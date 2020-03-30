cf6332c7df50eb4a12052f8572f572f2395b1212
pcchiu96 <pcchiu96@gmail.com>
2020-03-29 23:59:42 -0700

    added piece drop feature

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import Board from "./Board.js";
 import "./App.css";
 
 export default function App() {
-    const [state, setState] = useState();
     const x = 7,
         y = 6;
-    const board = Array(x).fill(Array(y).fill(0)); //[x][y]
+    const b = Array(x)
+        .fill(0)
+        .map(() => Array(y).fill(0));
+    const [board, setBoard] = useState([]);
 
-    function handleClick(i) {
-        console.log(i);
+    useEffect(() => {
+        setBoard(b);
+    }, []);
+
+    let turn = "X";
+
+    function updateBoard(i) {
+        let newBoard = [...board];
+        for (let j = newBoard[i].length - 1; j >= 0; j--) {
+            if (newBoard[i][j] === 0) {
+                newBoard[i][j] = turn;
+                break;
+            }
+        }
+        setBoard(newBoard);
+        //console.log(board);
     }
 
     return (
         <div className='board'>
-            <Board board={board} />
+            <Board board={board} updateBoard={updateBoard} />
         </div>
     );
 }
--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Square from "./Square";
 
-export default function Board({ board }) {
+export default function Board({ board, updateBoard }) {
     function generateColumns(board) {
         return board.map((row, index) => {
             return (
                 <div className='column' value={index}>
-                    {generateRows(board[index])}
+                    {generateHeight(board[index], index)}
                 </div>
             );
         });
     }
 
-    function generateRows(board) {
+    function generateHeight(board, j) {
         return board.map((column, index) => {
             return (
                 <>
-                    <button value={index} onClick={() => columnClick(index)}>
+                    <button value={j} onClick={handleClick}>
                         {column}
                     </button>
                     <br />
@@ -25,8 +25,9 @@ export default function Board({ board }) {
         });
     }
 
-    function columnClick(j) {
-        console.log(j);
+    function handleClick(event) {
+        updateBoard(event.target.value);
+        console.log(event.target.value);
     }
 
     return <div className='board'>{generateColumns(board)}</div>;
